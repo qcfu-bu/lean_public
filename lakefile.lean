9f9e0e2c6d46a8c8b348a8a26902f21d4d5d67a6
Qiancheng Fu <qcfu@bu.edu>
2025-08-30 16:32:06 -0400

    ok

---
+++ lakefile.lean
@@ -0,0 +1,18 @@
+import Lake
+
+open Lake DSL
+
+def extraArgs := #["-DautoImplicit=false", "-Dlinter.unusedVariables=false"]
+
+package mytest {
+  moreGlobalServerArgs := extraArgs
+}
+
+@[default_target]
+lean_lib MyTest {
+  roots := #[`MyTest]
+  globs := #[Glob.submodules `MyTest]
+  moreLeanArgs := extraArgs
+}
+
+require autograder from git "https://github.com/robertylewis/lean4-autograder-main"
